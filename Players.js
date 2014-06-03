451a550bffc4d77be9aeac702fd421b4b5b40c5f
amitmiran <amitmiran@gmail.com>
2014-06-03 17:58:35 +0300

    Factory file separation

--- Players.js
+++ Players.js
@@ -7,13 +7,20 @@ angular.module("HexGame")
         var players = ["red", "blue"];
         var usedPlayers = 0;
         return {
-            setPlayerTurn: function (player) {
-                this.currentPlayer = player;
-            },
-            getAvailablePlayers: function () {
+            getAvailablePlayer: function () {
                 var player = players[usedPlayers];
                 usedPlayers++;
                 return player;
+            },
+            isPlayer: function (hex) {
+
+                var isPlayer = false;
+                angular.forEach(players, function (player) {
+                    if (hex.state == player) {
+                        isPlayer = true;
+                    }
+                });
+                return isPlayer;
             }
-        }
+        };
     });
